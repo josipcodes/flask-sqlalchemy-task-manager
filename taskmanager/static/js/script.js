af80e64c41fb81227b3018c429ffc9763eeac396
Josip Potnar <j.potnar@gmail.com>
2023-10-22 15:35:08 +0000

    Add reading functionality for tasks

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -13,5 +13,9 @@ document.addEventListener('DOMContentLoaded', function() {
   // select initialization
   var selects = document.querySelectorAll('select');
   M.FormSelect.init(selects);
+
+  // collapsible initialization
+  var collapsibles = document.querySelectorAll('.collapsible');
+  M.Collapsible.init(collapsibles);
 });
   

0cdcc3666879f3aed1563012e97d2c09d8174aa7
Josip Potnar <j.potnar@gmail.com>
2023-10-22 15:02:43 +0000

    Creade add task functionality

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,5 +1,17 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // sidenav initialization
-    var sidenav = document.querySelectorAll('.sidenav');
-    M.Sidenav.init(sidenav);
-  });
+  // sidenav initialization
+  var sidenav = document.querySelectorAll('.sidenav');
+  M.Sidenav.init(sidenav);
+
+  // datepicker initialization
+  var datepicker = document.querySelectorAll('.datepicker');
+  M.Datepicker.init(datepicker, {
+    format: "dd mmm, yyyy",
+    i18n: {done: "Yaaaas"}
+    });
+
+  // select initialization
+  var selects = document.querySelectorAll('select');
+  M.FormSelect.init(selects);
+});
+  
